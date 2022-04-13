a9f3539c6955967fc4c928826877b82ab5479545
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-04-13 14:43:58 +0900

    basics

--- app.py
+++ app.py
@@ -1,5 +1,42 @@
-icecream = "so cool"
+# icecream = "so cool"
 
-print(icecream)
-print(icecream[0:4])
-print(icecream[1])
+# print(icecream)
+# print(icecream[0:4])
+# print(icecream[1])
+
+# fruits = ['strawberry', 'apple', 'pear', 'plum'];
+
+# if 'pear' in fruits:
+#     print('You have a pear')
+
+# for i in range(0, 10):
+#     print(i, 'is a number')
+
+# for fruit in fruits:
+#     print(fruit)
+
+# a = 'hello'
+# b = 'there'
+# c = a + b
+
+# print(a + b)
+
+# a = 1
+# b = str(a);
+# c = ' is a number'
+# print(b + c)
+
+# def plus(a, b):
+#     print(type(a), type(b))
+#     if type(a) == int and type(b) == int:
+#         return a + b
+#     else:
+#         return None
+
+# sum = plus(1, 3)
+
+# print(sum)
+
+# from math import ceil
+
+# print(ceil(3.7))
